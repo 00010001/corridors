b17daa090f482dabb2f276ebd02e7a798d4c78f9
00010001 <Koalicja14?>
2020-12-21 23:41:42 +0100

    fixwhennewgameafterdeath

--- base/src/main/java/com/sample/base/factory/LevelFactory.java
+++ base/src/main/java/com/sample/base/factory/LevelFactory.java
@@ -1,7 +1,9 @@
 package com.sample.base.factory;
 
-import com.sample.base.model.MapData;
 import com.sample.base.model.Level;
+import com.sample.base.model.MapData;
+
+import java.util.Arrays;
 
 public class LevelFactory {
 
@@ -13,17 +15,21 @@ public class LevelFactory {
                         .id(-1)
                         .startingRow(2)
                         .startingCol(2)
-                        .map(MapData.TEST_MAP);
+                        .map(cloneMapMatrix(MapData.TEST_MAP));
             case 0:
                 return new Level()
                         .id(0)
                         .startingRow(5)
                         .startingCol(1)
-                        .map(MapData.MAP_0);
+                        .map(cloneMapMatrix(MapData.MAP_0));
             default:
                 throw new IllegalArgumentException("level number not present");
         }
 
+    }
+
+    private static int[][] cloneMapMatrix(int[][] matrix) {
+        return Arrays.stream(matrix).map(int[]::clone).toArray(int[][]::new);
     }
 
 }
