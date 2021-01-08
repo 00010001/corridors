776f8bdeab6849d054832dad413e9082e5617245
00010001 <Koalicja14?>
2021-01-08 11:39:37 +0100

    linux clear console screen

--- console/src/main/java/com/sample/console/renderer/service/ConsoleService.java
+++ console/src/main/java/com/sample/console/renderer/service/ConsoleService.java
@@ -6,7 +6,11 @@ public class ConsoleService {
 
     public void clearScreen() {
         try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            if (System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+            }
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
